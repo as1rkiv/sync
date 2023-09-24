50a2d8c0770b92bd015100cbc788e7764c244e87
as1rkiv <as1rkiv@outlook.com>
2023-09-24 12:15:00 +0800

    modified:   surge/biqugei.net/ads.js

--- surge/biqugei.net/ads.js
+++ surge/biqugei.net/ads.js
@@ -2,5 +2,5 @@ const { body } = $response;
 const ctx = body
   .replace(/(\<script\s?(type\=\"text\/javascript\")?\s?\>\s?\(function\(.*?\)\s?\{.*?\}\)\(.*?\)\;\s?\<\/script\>)/gims, '')
   .replace(/(\<script\>if\(.*test\(.*\)\)\{.*\}\;\<\/script\>)/gims, '')
-  .replace(/\<script\>\!function\(\)\{.*new\sFunction\(\w\)\(\)\}\(\)\;\<\/script\>/gims, '');
+  .replace(/\<script\>\!function\(\)\{.*\snew\sFunction\(\w\)\(\)\}\(\)\;\<\/script\>/gims, '');
 $done({ body: ctx });
