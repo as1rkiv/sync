78c5d5ace5bfb3a40e92eb0a6164c2cf3b60c223
as1rkiv <as1rkiv@outlook.com>
2023-10-12 14:43:36 +0800

    modified:   README.md 	deleted:    surge/biqugei.net/ads.js 	deleted:    surge/biqugei.net/ads.sgmodule

--- README.md
+++ README.md
@@ -4,5 +4,4 @@
 |App|名称|raw|描述|
 |:-:|:-:|:-:|:-:|
 |AdGuard|`blocklist`|[link](https://raw.githubusercontent.com/as1rkiv/sync/main/adguard/blocklist.txt)|屏蔽页面元素
-|Surge|`biqugei.net`|[link](https://raw.githubusercontent.com/as1rkiv/sync/main/surge/biqugei.net/ads.sgmodule)|笔趣阁去广告
 
--- surge/biqugei.net/ads.js
+++
@@ -1,6 +0,0 @@
-const { body } = $response;
-const ctx = body
-  .replace(/(\<script\s?(type\=\"text\/javascript\")?\s?\>\s?\(function\(.*?\)\s?\{.*?\}\)\(.*?\)\;\s?\<\/script\>)/gims, '')
-  .replace(/(\<script\>if\(.*test\(.*\)\)\{.*\}\;\<\/script\>)/gims, '')
-  .replace(/\<script\>\!function\(\)\{.*\snew\sFunction\(\w\)\(\)\}\(\)\;\<\/script\>/gims, '');
-$done({ body: ctx });
